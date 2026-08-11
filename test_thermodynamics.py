f454e78acf8260336738ce3477194df793282091
fedesa belete <fedesabelete72@gmail.com>
2026-08-11 17:05:29 +0300

    Clean up duplicate modules and obsolete tests

--- test_thermodynamics.py
+++
@@ -1,9 +0,0 @@
-from engineering.thermodynamics import Thermodynamics
-
-thermo = Thermodynamics()
-
-print(thermo.ideal_gas_law(101325, 1, 300))
-
-print(thermo.boyles_law(100000, 2, 1))
-
-print(thermo.specific_heat(2, 4184, 20))
